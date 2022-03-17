d5153ed7c1dfc8a3d68505613222047188556f9f
ratchet11243 <rphodgkinson@gmail.com>
2022-03-17 14:26:59 +0100

    Merge branch 'main' of https://github.com/AAUSmartProductionLab/PLM-demonstrator

--- scripts/test_getProdID.py
+++ scripts/test_getProdID.py
@@ -1,18 +1,17 @@
 import os
-from festo_opcua import getProdID
+from festo_opcua import simpleConnect
 
 IP = '172.20.1.1'
 
 def main():
-    PNo = getProdID(IP)
+    dickhead = simpleConnect()
+    PNo = dickhead.getProdID(IP)
     print(PNo)
 
 if __name__ == "__main__":
-    try:    
-       main()
-
-    except(KeyboardInterrupt):
-        print('interrupted!')
-        os.exit()
-
-
+    while True:
+        try:    
+            main() 
+        except(KeyboardInterrupt):
+            print('interrupted!')
+            os._exit()
